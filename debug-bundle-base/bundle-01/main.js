e944806a4201154bce4befebcbccffe731852dbb
Cristian1231233 <campagnacristian22@gmail.com>
2021-12-09 17:58:09 +0100

    commit

---
+++ debug-bundle-base/bundle-01/main.js
@@ -0,0 +1,100 @@
+/*******************************************************************************
+
+    Per ognuno dei seguenti esercizi, rispondi a queste domande:
+    1- Che cosa fa questo codice?
+    2- Sono presenti errori di sintassi?
+    3- Sono presenti errori logici?
+
+    Inserisci i tuoi commenti con le risposte, sotto ciascun esercizio
+
+*******************************************************************************/
+
+
+// ESERCIZIO 1
+// for (let i = 0; i > 5; i++) {
+//     console.log(i);
+// }
+
+// 1- Fa un ciclo di numeri da 0 a 5
+// 2- Si, i > 5
+// 3- No
+
+// correzione
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+
+// ESERCIZIO 2
+// function addIfEven(num) {
+//     if (num % 2 = 0) {
+//         return num + 5;
+//     }
+//     return num;
+// }
+// 1- Fa una funzione che riceve un numero come parametro e in base alla condizione restituisce un risultato. Se il numero è divisibile per 2 restituisce il numero più 5 altrimenti restituisce il numero
+// 2- Si, 2 = 0 
+// 3- Si, la funzione deve essere invocata
+
+// correzione
+const num = 4;
+
+console.log(addIfEven(4));
+
+function addIfEven(num) {
+    if (num % 2 === 0) {
+        return num + 5;
+    }
+    return num;
+}
+
+// ESERCIZIO 3
+// function loopToFive() {
+//     for (let i = 0, i < 5, i++) {
+//         console.log(i);
+//     }
+// }
+
+// 1- Fa una funzione che cicla i numeri da 0 a 5
+// 2- Si, le virgole nella struttura del ciclo for
+// 3- Si, la funzione deve essere invocata
+
+loopToFive();
+function loopToFive() {
+    for (let i = 0; i < 5; i++) {
+        console.log(i);
+    }
+}
+
+// ESERCIZIO 4 (suggerimento: ci sono 7 errori)
+// function displayEvenNumbers() {
+//     let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+//     let evenNumbers = [];
+//     for (let i = 0; i < numbers.length - 1; i++;) {
+//         if (numbers % 2 = 0); {
+//             evenNumbers.push(i);
+//         }
+//         return evenNumbers;
+//     }
+// }
+// displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
+
+// 1- Fa una funzione in cui ci sono due array, uno con dei numeri e l'altro vuoto. La funzione cicla i numeri del primo array e inserisce nel secondo solo i pari
+// 2- Si, i++; , 2 = 0); 
+// 3- No
+
+//////////////////
+
+function displayEvenNumbers() {
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+    let evenNumbers = [];
+    for (let i = 0; i < numbers.length - 1; i++) {
+        if (numbers % 2 === 0) {
+           return evenNumbers.push(i);
+        }
+        return evenNumbers;
+    }
+}
+displayEvenNumbers();
+console.log(displayEvenNumbers());
---
+++ debug-bundle-base/bundle-02/main.js
@@ -0,0 +1,187 @@
+/*******************************************************************************
+​
+    Per ognuno dei seguenti esercizi, rispondi a queste domande:
+    1- Che cosa fa questo codice?
+    2- Sono presenti errori di sintassi?
+    3- Sono presenti errori logici?
+​
+    Inserisci i tuoi commenti con le risposte alla fine dell'esercizio
+​
+*******************************************************************************/
+
+
+// ESERCIZIO 1
+// function checkAge() {
+//     const myAge = 32;
+//     const message = '';
+
+//     if (myAge < 18) {
+//         message = `Sei troppo giovane! Hai ${myAge} anni!`;
+//     } else {
+//         message = 'Hai più di 18 anni!';
+//     }
+// }
+// checkAge();
+
+// 1- Fa una funzione che stampa in consol un messaggio in base ad una condizione
+// 2- Si, message deve essere let
+// 3- No
+
+function checkAge() {
+    const myAge = 32;
+    let message = '';
+
+    if (myAge < 18) {
+        message = `Sei troppo giovane! Hai ${myAge} anni!`;
+        console.log(message);
+    } else {
+        message = 'Hai più di 18 anni!';
+        console.log(message);
+    }
+}
+checkAge();
+///////////////////
+
+// ESERCIZIO 2
+// function printColorsNumber() {
+//     const colors = ['blue', 'red', 'yellow', 'green', 'black'];
+//     console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
+// }
+// printColorsNumber();
+
+// 1- Fa una funzione che ci dice quanti elementi ci cono nell'array
+// 2- Si, lenght
+// No
+
+function printColorsNumber() {
+    const colors = ['blue', 'red', 'yellow', 'green', 'black'];
+    console.log(`Nella mia palette ci sono ${colors.length} colori!`);
+}
+printColorsNumber();
+
+//////////////////
+
+// ESERCIZIO 3
+// function addNumbers() {
+//     const userNumber = prompt('Inserisci un numero');
+//     const total = userNumber + 12;
+
+//     console.log(`Il risultato finale è ${total}`);
+// }
+// addNumbers();
+
+// 1- Fa una funzione che somma ad un numero inserito dall'utente un altro numero
+// 2- Si, per fare la somma dobbiamo inserire parseInt
+// 3- No
+
+function addNumbers() {
+    const userNumber = parseInt(prompt('Inserisci un numero'));
+    const total = userNumber + 12;
+
+    console.log(`Il risultato finale è ${total}`);
+}
+addNumbers();
+
+//////////////////
+
+// ESERCIZIO 4
+// function checkAccess() {
+//     const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
+//     const userEmail = prompt('Inserisci il tuo indirizzo email');
+
+//     let grantAccess = 'false';
+
+//     if (addresses.includes(userEmail)) {
+//         grantAccess = 'true';
+//     }
+
+//     if (grantAccess === true) {
+//         console.log('Accesso consentito!');
+//     } else {
+//         console.log('Accesso negato!');
+//     }
+// }
+// checkAccess();
+
+// 1- Fa una funzione che controlla con una condizione se l'email inserita e nella lista
+// 2- Si, il valore booleano non deve avere le virgolette
+// 3- No
+
+///////////////////
+
+function checkAccess() {
+    const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
+    const userEmail = prompt('Inserisci il tuo indirizzo email');
+
+    let grantAccess = false;
+
+    if (addresses.includes(userEmail)) {
+        grantAccess = true;
+    }
+
+    if (grantAccess === true) {
+        console.log('Accesso consentito!');
+    } else {
+        console.log('Accesso negato!');
+    }
+}
+checkAccess();
+
+////////////////////
+
+// ESERCIZIO 5 (suggerimento: c'è un solo errore)
+function checkAccessImproved() {
+    const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
+
+    const userEmail = prompt('Inserisci il tuo indirizzo email');
+
+    let grantAccess = false;
+
+    for (let i = 0; i < addresses.length; i++) {
+     const email = addresses[i];
+        
+      if (userEmail.length > 5) {
+            
+        if (email === userEmail) {
+        grantAccess = 'true';            
+            
+      }
+        
+    }
+
+    if (grantAccess) {
+        console.log('Accesso consentito!');
+    } else {
+        console.log('Accesso negato!');
+    }
+}
+checkAccessImproved();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
